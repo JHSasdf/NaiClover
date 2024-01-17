7bca971521944200ff8c8cb3b3b6c8f946ccc872
wjdgns4070 <ljj888800@naver.com>
2024-01-17 12:16:11 +0900

    feature socket

--- server/app.ts
+++ server/app.ts
@@ -1,40 +1,51 @@
-import express from 'express';
-import { Express, Request, Response } from 'express';
-const app: Express = express();
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(cors());
 
-let id = 2;
-const todoList = [
-  {
-    id: 1,
-    text: '할일1',
-    done: false,
-  },
-];
+const connectedClients: Record<string, Socket> = {};
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/App.tsx');
+});
+
+io.on('connection', (socket: Socket) => {
+  console.log('a user connected');
+
+  // 클라이언트 소켓 저장
+  connectedClients[socket.id] = socket;
+
+  // 클라이언트로부터 받은 메시지를 모든 클라이언트에게 전송
+  socket.on('chat message', (msg: string) => {
+    io.emit('chat message', { id: socket.id, message: msg });
+  });
+
+  // 클라이언트가 연결을 끊을 때
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    // 연결이 끊긴 클라이언트 소켓 삭제
+    delete connectedClients[socket.id];
+  });
+});
 
 app.get('/', function (req: Request, res: Response) {
   res.send('hello');
 });
 
-app.get('/api/todo', (req: Request, res: Response) => {
-  res.json(todoList);
-});
-
-app.post('/api/todo', (req: Request, res: Response) => {
-  const { text, done } = req.body;
-  todoList.push({
-    id: id++,
-    text,
-    done,
-  });
-  return res.send('succes');
-});
-
-app.listen(4000, () => {
-  console.log('server open');
+const PORT = 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
