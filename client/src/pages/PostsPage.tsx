993679021cc49a042161643dd0a19750b81f6fec
cyunlee <siyunkr@gmail.com>
2024-01-18 09:38:47 +0900

    postpage

--- client/src/pages/PostsPage.tsx
+++ client/src/pages/PostsPage.tsx
@@ -1,10 +1,14 @@
 import Post from '../components/postspage/Post';
 import Search from '../components/postspage/Search';
+import Header from '../components/postspage/Header';
+import Category from '../components/postspage/Category';
 
 function PostsPage() {
   return (
     <>
+      <Header />
       <Search />
+      <Category />
       <Post />
       <Post />
     </>
