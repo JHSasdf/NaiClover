ea583d494ef18e4780564ce0ef65be096bddaeeb
JHSasdf <sgh4760@naver.com>
2024-01-18 11:38:21 +0900

    reconstructureing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
-import MainPage from './pages/MainPage.tsx';
-import NewPage from './pages/NewPage.tsx';
 import PostsPage from './pages/PostsPage.tsx';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -30,36 +28,10 @@ function App() {
 
   const [cookies, setCookies, removeCookies] = useCookies(['id']);
 
-  function login() {
-    setCookies('id', true, cookieConfig);
-  }
-
-  function logout() {
-    removeCookies('id', cookieConfig);
-  }
-  function check() {
-    console.log(cookies.id);
-  }
-
   return (
     <div className="App">
-      <button onClick={check}>로그아웃</button>
-      {todoList.map((elem: any) => {
-        return (
-          <ul key={elem.id}>
-            <li>{elem.text} </li>
-            <li>{elem.id}</li>
-            <li>{JSON.stringify(elem.done)}</li>
-          </ul>
-        );
-      })}
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={<MainPage todoList={todoList}></MainPage>}
-          ></Route>
-          <Route path="/new" element={<NewPage></NewPage>}></Route>
           <Route path="/posts" element={<PostsPage></PostsPage>}></Route>
         </Routes>
       </BrowserRouter>
@@ -68,30 +40,3 @@ function App() {
 }
 
 export default App;
-
-// import React, { useEffect } from 'react';
-// import axios from 'axios';
-
-// function App() {
-//   useEffect(() => {
-//     // useEffect를 활용하여 컴포넌트가 마운트될 때 데이터를 가져오도록 합니다.
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get('http://localhost:4000/api/todo');
-//         console.log(response.data);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []); // 빈 배열을 전달하여 한 번만 실행되도록 합니다.
-
-//   return (
-//     <div className="App">
-//       <h1>TodoList</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
