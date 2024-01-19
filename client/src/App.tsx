7e083076ea97b6183b61b165ed57efb4659eeb7c
JHSasdf <sgh4760@naver.com>
2024-01-19 11:35:46 +0900

    feat_loginPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,5 @@
-//fetch
-//axios
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 
 import PostsPage from './pages/PostsPage.tsx';
 import SignupPage from './pages/SignupPage.tsx';
@@ -10,28 +7,14 @@ import NewPostPage from './pages/NewPostPage.tsx';
 
 import { v4 as uuidv4 } from 'uuid';
 
-import { useCookies } from 'react-cookie';
-import { cookieConfig } from './utils/cookieConfig.ts';
 import Mypage from './pages/Mypage.tsx';
+import LoginPage from './pages/LoginPage.tsx';
 
 export const generateUniqueId = () => {
     return uuidv4();
 };
 
 function App() {
-    // 객체로 설정된 초기 상태값을 배열로 변경
-    // 서버에서 가져온 데이터는 React.ReactNode라는 속성이 되는 것 같음
-    const [todoList, setTodoList] = useState<React.ReactNode[]>([]);
-    useEffect(() => {
-        fetch('http://localhost:4000/api/todo')
-            .then((response) => response.json())
-            .then((data) => {
-                setTodoList(data);
-            });
-    }, []);
-
-    const [cookies, setCookies, removeCookies] = useCookies(['id']);
-
     return (
         <div className="App">
             <BrowserRouter>
@@ -42,10 +25,8 @@ function App() {
                     ></Route>
                     <Route path="/posts" element={<PostsPage />}></Route>
                     <Route path="/mypage" element={<Mypage />} />
-                    <Route
-                        path="/signup"
-                        element={<SignupPage></SignupPage>}
-                    ></Route>
+                    <Route path="/signup" element={<SignupPage />}></Route>
+                    <Route path="/login" element={<LoginPage />}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
--- client/src/utils/cookieConfig.ts
+++ client/src/utils/cookieConfig.ts
@@ -1,6 +1,6 @@
 export const cookieConfig: Object = {
-  path: '/',
-  // 단위는 초
-  maxAge: 30 * 60,
-  httponly: true,
+    path: '/',
+    // 단위는 초
+    maxAge: 2 * 60 * 60,
+    httponly: true,
 };
