c1a081af98cea2caae4a318401c04763d4caea56
Hanwoori00 <729han@gmail.com>
2024-01-22 09:39:08 +0900

    followFunc

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,6 +1,15 @@
 // 여러 파일에서 공통적으로 사용되는 타입을 모아서 관리
 export interface TodoItemProp {
-  id: number;
-  content: string;
-  done: boolean;
+    id: number;
+    content: string;
+    done: boolean;
+}
+
+export interface User {
+    userid: string;
+    password: string;
+    name: string;
+    gender: string;
+    nation: string;
+    firLang: string;
 }
