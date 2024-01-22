b0a0ef339a97f7f5fbfcb30e98d58511830dd6af
kimjaehyeon0527 <kimdh66665@naver.com>
2024-01-22 11:26:24 +0900

    시윤씨 post 버튼 부분 수정 완료

--- client/src/pages/PostsPage.tsx
+++ client/src/pages/PostsPage.tsx
@@ -9,62 +9,66 @@ import '../styles/PostsPage.scss';
 import Footer from '../components/Footer';
 
 function PostsPage() {
-  const [showLanguagePosts, setShowLanguagePosts] = useState(true);
-  const [showCulturePosts, setShowCulturePosts] = useState(false);
+    const [showLanguagePosts, setShowLanguagePosts] = useState(true);
+    const [showCulturePosts, setShowCulturePosts] = useState(false);
 
-  const handleLanguageClick = () => {
-    setShowLanguagePosts(!showLanguagePosts);
-    setShowCulturePosts(false);
-  };
+    const handleLanguageClick = () => {
+        setShowLanguagePosts(!showLanguagePosts);
+        setShowCulturePosts(false);
+    };
 
-  const handleCultureClick = () => {
-    setShowLanguagePosts(false);
-    setShowCulturePosts(!showCulturePosts);
-  };
+    const handleCultureClick = () => {
+        setShowLanguagePosts(false);
+        setShowCulturePosts(!showCulturePosts);
+    };
 
-  return (
-    <div className="postspage-container">
-      <Topbar />
-      <Header />
-      <Search />
-      <div className='category-component'>
-        <button
-          className={`btn_lang ${showLanguagePosts ? 'active' : ''}`}
-          onClick={handleLanguageClick}
-          style={showLanguagePosts ? { borderBottomColor: '#000', borderBottom: '2px solid', color: '#000' } : {}}
-        >
-          언어
-        </button>
-        <br />
-        <button
-          className={`btn_culture ${showCulturePosts ? 'active' : ''}`}
-          onClick={handleCultureClick}
-          style={showCulturePosts ? { borderBottomColor: '#000', borderBottom: '2px solid', color: '#000' } : {}}
-        >
-          문화
-        </button>
-        <br />
-      </div>
+    return (
+        <div className="postspage-container">
+            <Topbar />
+            <Header />
+            <Search />
+            <div className="category-component">
+                <div
+                    className={`btn_lang ${
+                        showLanguagePosts
+                            ? 'active category-component-changed'
+                            : ''
+                    }`}
+                    onClick={handleLanguageClick}
+                >
+                    언어
+                </div>
+                <br />
+                <div
+                    className={`btn_culture ${
+                        showCulturePosts
+                            ? 'active category-component-changed'
+                            : ''
+                    }`}
+                    onClick={handleCultureClick}
+                >
+                    문화
+                </div>
+                <br />
+            </div>
 
-      {showLanguagePosts && (
-        <div className='language-posts-container'>
-          <LanguagePost />
-          <LanguagePost />
-          <LanguagePost />
+            {showLanguagePosts && (
+                <div className="language-posts-container">
+                    <LanguagePost />
+                    <LanguagePost />
+                    <LanguagePost />
+                </div>
+            )}
+
+            {showCulturePosts && (
+                <div className="culture-posts-container">
+                    <CulturePost />
+                </div>
+            )}
+
+            <Footer />
         </div>
-      )}
-
-      {showCulturePosts && (
-        <div className="culture-posts-container">
-          <CulturePost />
-        </div>
-      )}
-
-      <Footer />
-    </div>
-  );
+    );
 }
 
 export default PostsPage;
-
-
