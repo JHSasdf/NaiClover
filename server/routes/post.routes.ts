4b48377bdda07619c0355293665a851231665233
JHSasdf <sgh4760@naver.com>
2024-01-25 12:30:27 +0900

    bug_fix_App.ts_backend routes

--- server/routes/post.routes.ts
+++ server/routes/post.routes.ts
@@ -2,22 +2,22 @@ import express from 'express';
 import * as controller from '../controllers/post.controller';
 export const postsRouter = express();
 
-postsRouter.get('/posts', controller.getPosts);
+postsRouter.get('/cul/posts', controller.getPosts);
 
-postsRouter.post('/posts/createpost', controller.createPost);
+postsRouter.post('/cul/posts/createpost', controller.createPost);
 
-postsRouter.get('/posts/:id', controller.getSinglePost);
+postsRouter.get('/cul/posts/:id', controller.getSinglePost);
 
-postsRouter.patch('/posts/:id', controller.updatePost);
+postsRouter.patch('/cul/posts/:id', controller.updatePost);
 
-postsRouter.delete('/posts/:id', controller.deletePost);
+postsRouter.delete('/cul/posts/:id', controller.deletePost);
 
-postsRouter.post('/posts/:id', controller.togglePostLike);
+postsRouter.post('/cul/posts/:id', controller.togglePostLike);
 
-postsRouter.post('/comments/createcomment/:id', controller.createComment);
+postsRouter.post('/cul/comments/createcomment/:id', controller.createComment);
 
-postsRouter.get('/comments/:id', controller.getComments);
+postsRouter.get('/cul/comments/:id', controller.getComments);
 
-postsRouter.patch('/comments/:commentindex', controller.updateComment);
+postsRouter.patch('/cul/comments/:commentindex', controller.updateComment);
 
-postsRouter.delete('/comments/:commentindex', controller.deleteComment);
+postsRouter.delete('/cul/comments/:commentindex', controller.deleteComment);
