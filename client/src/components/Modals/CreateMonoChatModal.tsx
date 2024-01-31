4af2454d8055adefc58e04ae9be8f1b03175e07e
kimjaehyeon0527 <kimdh66665@naver.com>
2024-01-31 10:34:37 +0900

    monoChat modal 연결

--- client/src/components/Modals/CreateMonoChatModal.tsx
+++ client/src/components/Modals/CreateMonoChatModal.tsx
@@ -2,13 +2,40 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import '../../styles/CreateMonoChatModal.scss';
+import { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
 
 function CreateMonoChatModal({ show, setShow, navigate }: any) {
+    const [newRoomName, setNewRoomName] = useState<string>('');
+    const [restrictedLang, setRestrictLang] = useState<string | null>(null);
+    const [cookies] = useCookies(['id']);
+    const userid = cookies['id'];
+
     const handleClose = () => {
         setShow({ show: false });
-        navigate('');
+    };
+    // 방 추가 함수
+    const handleAddRoom = () => {
+        if (newRoomName.trim() !== '') {
+            socket.emit('createRoom', {
+                roomName: newRoomName,
+                userid: userid,
+                useridTo: 'monoChat',
+                restrictedLang: restrictedLang, // 이 부분 수정
+            });
+            setNewRoomName('');
+            handleClose();
+        }
     };
 
+    const handleRoomCreated = ({ roomNum }: { roomNum: string }) => {
+        // 방이 생성되면 해당 방으로 이동
+        window.location.href = `/chat/${roomNum}`;
+    };
+    socket.on('roomCreated', handleRoomCreated);
     return (
         <>
             {/* 변경 완료 되었을 시에 모달! */}
@@ -32,8 +59,10 @@ function CreateMonoChatModal({ show, setShow, navigate }: any) {
                             <Form.Label>Room Name</Form.Label>
                             <Form.Control
                                 type="text"
-                                placeholder=""
+                                placeholder="Enter new room name"
                                 autoFocus
+                                value={newRoomName}
+                                onChange={(e) => setNewRoomName(e.target.value)}
                             />
                         </Form.Group>
                         <Form.Group
@@ -41,13 +70,19 @@ function CreateMonoChatModal({ show, setShow, navigate }: any) {
                             controlId="exampleForm.ControlTextarea1"
                         >
                             <Form.Label>Choose Language</Form.Label>
-                            <Form.Control as="select">
-                                <option value="">Korean</option>
-                                <option value="">English</option>
-                                <option value="">Chinese</option>
-                                <option value="">Japanese</option>
-                                <option value="">French</option>
-                                <option value="">German</option>
+                            <Form.Control
+                                as="select"
+                                value={restrictedLang || ''}
+                                onChange={(e) =>
+                                    setRestrictLang(e.target.value)
+                                }
+                            >
+                                <option value="Korean">Korean</option>
+                                <option value="English">English</option>
+                                <option value="Chinese">Chinese</option>
+                                <option value="Japanese">Japanese</option>
+                                <option value="French">French</option>
+                                <option value="German">German</option>
                             </Form.Control>
                         </Form.Group>
                     </Form>
@@ -65,9 +100,7 @@ function CreateMonoChatModal({ show, setShow, navigate }: any) {
                     <Button
                         style={{ backgroundColor: '#56eebbb3', color: 'black' }}
                         variant="secondary"
-                        onClick={() => {
-                            handleClose();
-                        }}
+                        onClick={handleAddRoom}
                     >
                         Create
                     </Button>
